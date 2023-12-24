c2f54fabb345651a43646b05e5cd7ebce23a6a74
Russ McKendrick <russ@mckendrick.io>
2023-12-24 11:16:11 +0000

    🔧 chore(generate_blog_post.py): refactor get_gpt3_text function to improve readability and maintainability 🔧 chore(generate_blog_post.py): remove unnecessary role and content keys from messages list in get_gpt3_text function

--- generate_blog_post.py
+++ generate_blog_post.py
@@ -32,13 +32,10 @@ def generate_random_number():
 
 # Function to get GPT-4 generated text
 def get_gpt3_text(prompt):
-    completion = client.completions.create(engine='gpt-4-1106-preview',
-    messages=[
-        {
-            'role': 'user',
-            'content': prompt
-        }
-    ])
+    completion = client.completions.create(
+    engine='gpt-4-1106-preview',
+    prompt=prompt
+    )
     return completion['choices'][0]['message']['content'].strip()
 
 # Get artist data from Last.fm API
